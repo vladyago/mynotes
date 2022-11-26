c9cd4083e6c01ca11588a8633897df0dee0656d6
Vlad Yagolnikov <vlad.yagolnikov@gmail.com>
2022-11-25 21:01:13 -0800

    step-27

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -150,4 +150,10 @@ class MockAuthProvider implements AuthProvider {
     );
     _user = newUser;
   }
+
+  @override
+  Future<void> sendPasswordReset({required String toEmail}) {
+    // TODO: implement sendPasswordReset
+    throw UnimplementedError();
+  }
 }
