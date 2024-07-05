1872243412949f1d0f194101dde38f2ac86f710f
Vlad Yagolnikov <vlad.yagolnikov@gmail.com>
2024-07-05 16:27:26 -0700

    updates build files to target android ver 14

--- lib/helpers/loading/loading_screen.dart
+++ lib/helpers/loading/loading_screen.dart
@@ -33,8 +33,8 @@ class LoadingScreen {
     required BuildContext context,
     required String text,
   }) {
-    final _text = StreamController<String>();
-    _text.add(text);
+    final streamText = StreamController<String>();
+    streamText.add(text);
 
     final state = Overlay.of(context);
     final renderBox = context.findRenderObject() as RenderBox;
@@ -66,7 +66,7 @@ class LoadingScreen {
                       const CircularProgressIndicator(),
                       const SizedBox(height: 20),
                       StreamBuilder(
-                        stream: _text.stream,
+                        stream: streamText.stream,
                         builder: (context, snapshot) {
                           if (snapshot.hasData) {
                             return Text(
@@ -92,12 +92,12 @@ class LoadingScreen {
 
     return LoadingScreenController(
       close: () {
-        _text.close();
+        streamText.close();
         overlay.remove();
         return true;
       },
       update: (text) {
-        _text.add(text);
+        streamText.add(text);
         return true;
       },
     );
